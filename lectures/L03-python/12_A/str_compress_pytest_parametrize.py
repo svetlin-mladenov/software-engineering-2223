567b08c2f2cc602b101814466d61c0730f8370ee
Svetlin Mladenov <smladenov@elsys-bg.org>
2022-11-03 14:25:51 +0200

    Add a version of str_compress with parametrized tests

---
+++ lectures/L03-python/12_A/str_compress_pytest_parametrize.py
@@ -0,0 +1,32 @@
+import string
+import pytest
+
+def compress(message):
+    new_list = ""
+
+    if len(message) < 2:
+        return message
+
+    count = 1
+    for i in range(len(message) - 1):
+        if message[i] == message[i+1]:
+            count += 1
+        else:
+            new_list += message[i] + str(count)
+            count = 1
+    new_list += message[i+1] + str(count)
+      
+    if(len(new_list) > len(message)):
+        return message
+    else:
+        return new_list
+
+@pytest.mark.parametrize("input, output", [
+    ("aabcccccaaa", "a2b1c5a3"),
+    ("aabccccca", "a2b1c5a1"),
+    ("abc", "abc"),
+    ("a", "a"),
+    ("Caseeee", "Caseeee")
+])
+def test_str_compress(input, output):
+    assert compress(input) == output
