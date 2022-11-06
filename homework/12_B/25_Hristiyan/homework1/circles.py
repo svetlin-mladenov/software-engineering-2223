2753b8d87dc2c78c3ab127b853f63e06b6e00b75
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2022-11-06 13:45:01 +0200

    add enum class and fix problems

--- homework/12_B/25_Hristiyan/homework1/circles.py
+++ homework/12_B/25_Hristiyan/homework1/circles.py
@@ -1,4 +1,13 @@
+from enum import Enum
 import math
+
+class Position(Enum):
+    pos1 = 'They are equal'
+    pos2 = 'They don\'t touch'
+    pos3 = 'They touch externally'
+    pos4 = 'They touch internally'
+    pos5 = 'They intersect at two points'
+    pos6 = 'One lying inside other'
 
 class Circle:
     def __init__(self, circle_name, X, Y, radius):
@@ -15,12 +24,18 @@ def mutual_position(first_circle, second_circle):
     radii_subtraction = abs(first_circle.radius - second_circle.radius)
     distance = distance_beetween_centers(first_circle, second_circle)
 
-    if distance == radii_subtraction ==  0: return "They are equal"
-    elif distance > radii_sum: return "They don't touch"
-    elif distance == radii_sum: return "They touch externally"
-    elif radii_subtraction < distance < radii_sum: return "They intersect at two points"
-    elif distance == radii_subtraction: return "They touch internally"
-    elif distance < radii_subtraction: return "One lying inside other"
+    if distance == radii_subtraction ==  0: 
+        return Position.pos1.value
+    elif distance > radii_sum: 
+        return Position.pos2.value
+    elif distance == radii_sum: 
+        return Position.pos3.value
+    elif distance == radii_subtraction: 
+        return Position.pos4.value
+    elif radii_subtraction < distance < radii_sum: 
+        return Position.pos5.value
+    elif distance < radii_subtraction: 
+        return Position.pos6.value
     
  
 print(f"Input for first circle")
--- homework/12_B/25_Hristiyan/homework1/test_circles.py
+++ homework/12_B/25_Hristiyan/homework1/test_circles.py
@@ -1,31 +1,31 @@
-import circles 
+import circles
 
-def test_circle():
-    first_circle = circles.Circle("first circle", -2.45, 1.9, 3.38)
-    second_circle = circles.Circle("second circle", -2.53, 2.64, 15.52)
-    assert circles.mutual_position(first_circle, second_circle) == "One lying inside other"
-
+def test_circle1():
+    first_circle = circles.Circle("first circle", 6, 5, 1)
+    second_circle = circles.Circle("second circle", 6, 5, 1)
+    assert circles.mutual_position(first_circle, second_circle) == circles.Position.pos1.value         
+    
 def test_circle2():
     first_circle = circles.Circle("first circle", 1, 1, 1)
     second_circle = circles.Circle("second circle", 6, 7, 1)
-    assert circles.mutual_position(first_circle, second_circle) == "They don't touch" 
+    assert circles.mutual_position(first_circle, second_circle) == circles.Position.pos2.value 
     
 def test_circle3():
-    first_circle = circles.Circle("first circle", -3.61, 5.06, 3.75)
-    second_circle = circles.Circle("second circle", -1.22, 4.13, 3.88)
-    assert circles.mutual_position(first_circle, second_circle) == "They intersect at two points"    
-
-def test_circle4():
     first_circle = circles.Circle("first circle", -5, 3, 1)
     second_circle = circles.Circle("second circle", -3, 3, 1)
-    assert circles.mutual_position(first_circle, second_circle) == "They touch externally" 
-
-def test_circle5():
+    assert circles.mutual_position(first_circle, second_circle) == circles.Position.pos3.value
+    
+def test_circle4():
     first_circle = circles.Circle("first circle", 6, 2.5, 1)
     second_circle = circles.Circle("second circle", 6, 2, 0.5)
-    assert circles.mutual_position(first_circle, second_circle) == "They touch internally"   
+    assert circles.mutual_position(first_circle, second_circle) == circles.Position.pos4.value 
     
+def test_circle5():
+    first_circle = circles.Circle("first circle", -3.61, 5.06, 3.75)
+    second_circle = circles.Circle("second circle", -1.22, 4.13, 3.88)
+    assert circles.mutual_position(first_circle, second_circle) == circles.Position.pos5.value   
+     
 def test_circle6():
-    first_circle = circles.Circle("first circle", 6, 5, 1)
-    second_circle = circles.Circle("second circle", 6, 5, 1)
-    assert circles.mutual_position(first_circle, second_circle) == "They are equal"         
+    first_circle = circles.Circle("first circle", -2.45, 1.9, 3.38)
+    second_circle = circles.Circle("second circle", -2.53, 2.64, 15.52)
+    assert circles.mutual_position(first_circle, second_circle) == circles.Position.pos6.value
