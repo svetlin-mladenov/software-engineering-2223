a3e3397ec8f9aa713eedaad4312c73c538e67408
Liverpoolfan92 <zhelev.burislav@gmail.com>
2022-11-10 15:14:47 +0200

    Update test_circle.py
    add test for circles touching from the inside

--- homework/12_B/4_Borislav_Zhelev/homework_circle/test_circle.py
+++ homework/12_B/4_Borislav_Zhelev/homework_circle/test_circle.py
@@ -7,6 +7,7 @@ def test_circle():
     
     assert are_touching(circle(1,1,5),circle(1,7,1))# 1 touching point
     assert are_touching(circle(1,1,5),circle(1,9,6))# two touching points
+    assert are_touching(circle(4,4,4),circle(2,4,2))# embedded but touching
     assert are_the_same(circle(1,1,5),circle(1,1,5))
     assert are_embedded(circle(1,1,5),circle(1,1,3))
     assert are_unrelated(circle(1,1,5),circle(100,100,1))
