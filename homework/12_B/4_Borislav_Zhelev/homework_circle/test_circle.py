77ebb04bf14b54cceb7eb06d28860816eada699b
Liverpoolfan92 <zhelev.burislav@gmail.com>
2022-11-10 15:05:20 +0200

    Update test_circle.py
    made the requested changes

--- homework/12_B/4_Borislav_Zhelev/homework_circle/test_circle.py
+++ homework/12_B/4_Borislav_Zhelev/homework_circle/test_circle.py
@@ -4,14 +4,10 @@ from are_embedded import *
 from are_unrelated import *
 from circle import *
 def test_circle():
-    c1 = circle(1,1,5)
-    c2 = circle(1,9,6)
-    c3 = circle(1,1,3)
-    c4 = circle(100,100,1)
-    c5 = circle(1,1,5)
-
-    assert are_touching(c1,c2)
-    assert are_the_same(c1,c5)
-    assert are_embedded(c1,c3)
-    assert are_unrelated(c1,c4)
-    assert not are_touching(c1,c4)
+    
+    assert are_touching(circle(1,1,5),circle(1,7,1))# 1 touching point
+    assert are_touching(circle(1,1,5),circle(1,9,6))# two touching points
+    assert are_the_same(circle(1,1,5),circle(1,1,5))
+    assert are_embedded(circle(1,1,5),circle(1,1,3))
+    assert are_unrelated(circle(1,1,5),circle(100,100,1))
+    assert not are_touching(circle(1,1,5),circle(100,100,1))
