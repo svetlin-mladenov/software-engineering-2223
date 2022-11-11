4da1f11426e7272f63e9f74732da4a93436f5f9b
Iliana Karakasheva <85765067+IlianaKarakasheva@users.noreply.github.com>
2022-11-11 15:42:33 +0200

    Create homework1.py

---
+++ homework/12_B/10_Iliana/homework1.py
@@ -0,0 +1,69 @@
+from enum import Enum
+import math
+
+class Circle:
+    def __init__(self, r, x, y):
+        self.r = r
+        self.x = x
+        self.y = y
+
+class Collison_type(Enum):
+    EQUAL = "Equal"
+    FINSIDE = "First in Second"
+    SINSIDE = "Second in First"
+    INTERSECT = "Intersect"
+    TOUCHING = "Touching"
+    NONE = "None"
+
+
+def getcollision_type(c1, c2):
+    d = math.sqrt((c1.x - c2.x) ** 2 + (c1.y - c2.y) ** 2)
+
+    if (c1.__dict__ == c2.__dict__):
+         return collison_type.EQUAL
+    if (d <= c1.r - c2.r):
+        return collison_type.SINSIDE
+    if (d <= c2.r - c1.r):
+        return collison_type.FINSIDE
+    if (d < c1.r + c2.r):
+        return collison_type.INTERSECT
+    if (d == c1.r + c2.r):
+        return collison_type.TOUCHING
+    else:
+        return collison_type.NONE
+
+def test_equal_circles():
+    c1 = Circle(10, 10, 10)
+    c2 = Circle(10, 10, 10)
+
+    assert getcollison_type(c1, c2) == collison_type.EQUAL
+
+def test_FINSIDE():
+    c1 = Circle(10, 0, 0)
+    c2 = Circle(20, 0, 0)
+
+    assert getcollison_type(c1, c2) == collison_type.FINSIDE
+
+def test_SINSIDE():
+    c1 = Circle(20, 0, 0)
+    c2 = Circle(10, 0, 0)
+
+    assert getcollison_type(c1, c2) == collison_type.SINSIDE
+
+def test_intersect():
+    c1 = Circle(10, 0, 0)
+    c2 = Circle(10, 10, 0)
+
+    assert getcollison_type(c1, c2) == collison_type.INTERSECT
+
+def test_touching():
+    c1 = Circle(10, -10, 0)
+    c2 = Circle(10, 10, 0)
+
+    assert getcollison_type(c1, c2) == collison_type.TOUCHING
+
+def test_none():
+    c1 = Circle(10, 0, 0)
+    c2 = Circle(10, 20, 20)
+
+    assert getcollison_type(c1, c2) == collison_type.NONE
