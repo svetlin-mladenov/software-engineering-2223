50467581f6c8f28eb8a87fc5676f1b403f208a93
Svetlin Mladenov <smladenov@elsys-bg.org>
2022-12-02 16:43:51 +0200

    Testing infrastructure. Test singup.

---
+++ otlichnik/tests/test_signup.py
@@ -0,0 +1,31 @@
+# pylint: disable=missing-function-docstring
+import pytest
+from otlichnik import create_app, init_db
+
+
+@pytest.fixture
+def app(tmp_path):
+    app = create_app({
+        "DATABASE": str(tmp_path / "otlichnik.db"),
+    })
+    with app.app_context():
+        init_db()
+    return app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_signup_get_page(client):
+    response = client.get("/signup")
+    assert response.status_code == 200
+
+
+def test_signup_new_user(client):
+    response = client.post("/signup", data={
+        "email": "mail@mail.com",
+        "password": "password",
+    })
+    assert response.headers["Location"] == "/"
