c513f48fb48bcd54abc7c5b41394299cd3e70e73
IliyanGavrilov <wardheromgrtoxic@gmail.com>
2023-01-06 09:59:29 +0200

    Added password validation check and 2 tests for valid and invalid passwords

--- otlichnik/otlichnik/__init__.py
+++ otlichnik/otlichnik/__init__.py
@@ -118,3 +118,7 @@ def init_db():
 def email_validation(email: str):
     email_regex = r'([A-Za-z0-9]+[.\-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)+'
     return re.fullmatch(email_regex, email)
+
+def password_validation(password: str):
+    password_regex = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,256}$'
+    return re.fullmatch(password_regex, password)
